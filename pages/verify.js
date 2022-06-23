bc286f3cceda61c7c1cb610fb32a7f8fab9a8e6d
Drunkenvalley <drunkenvalley@gmail.com>
2022-06-23 21:53:33 +0200

    Verify screen - Some mildly improved presentation

--- pages/verify.js
+++ pages/verify.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/layout'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useState } from 'react'
@@ -11,7 +12,7 @@ export default function Verify() {
     const [success, setSuccess] = useState('')
     useEffect(() => {
         if (!isReady) return
-        
+
         const hash = asPath.split('#')[1] || null
         if (hash) {
             setSuccess(hash)
@@ -19,11 +20,37 @@ export default function Verify() {
     }, [asPath, isReady])
 
     return (
-        <Layout>
-            <div>
-                {success}
+        <Layout mainClassName='flex justify-content-center align-items-center' direction="column">
+            <div className='flow max-w-400'>
+                {success && <>
+                    <h2>Successfully authorized</h2>
+                    <p>
+                        Thanks for authorizing. If not redirected you can click&nbsp;below.
+                    </p>
+                    <p className='border border-dark p-3 rounded-sm text-center'>
+                        <Link href='/dashboard'>
+                            <a className='flex-inline align-items-center text-purple'>
+                                Continue to dashboard
+                            </a>
+                        </Link>
+                    </p>
+                </>}
                 {error && <>
-                    {error_description}
+                    <h2>An error occurred</h2>
+                    <p>
+                        Twitch returned an error.<br />
+                        <code className='text-purple-200'>{error_description}</code><br />
+                    </p>
+                    <p>
+                        Please try again, or contact us if the problem persists.
+                    </p>
+                    <p className='border border-dark p-3 rounded-sm text-center'>
+                        <Link href='/'>
+                            <a className='flex-inline align-items-center text-purple'>
+                                Back to home
+                            </a>
+                        </Link>
+                    </p>
                 </>}
             </div>
         </Layout>
